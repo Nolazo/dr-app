75b4f58e4953c19a07918b00b147220a8687b0df
Nolazo <j.manuelvaldebenito@gmail.com>
2024-12-18 23:52:04 -0300

    first commit

---
+++ patients/views.py
@@ -0,0 +1,12 @@
+from .serializers import PatientSerializer
+from .models import Patient
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["GET"])
+def list_patients(request):
+    patients = Patient.objects.all()
+    serializer = PatientSerializer(patients, many=True)
+    return Response(serializer.data)
